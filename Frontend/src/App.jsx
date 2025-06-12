b74fdf0ea0852ee7040bfe5718044b8a2721e558
Rup-code <biswarupmaji.23@nshm.edu.in>
2025-06-12 11:55:36 +0530

    create a conference routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import { Routes , Route } from 'react-router-dom'
-import Home from './Paages/Home'
-import About from './Paages/About'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Paages/Home";
+import About from "./Paages/About";
+import Conference_track from "./Paages/Conference_track";
 
 function App() {
   return (
-    
     <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/about' element={<About/>} />
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/conference_track" element={<Conference_track />} />
     </Routes>
-
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ Frontend/src/Paages/Conference_track.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Conference_track() {
+  return (
+    <>
+    
+    <nav className='text-red-500'>Hello</nav>
+
+    </>
+  )
+}
+
+export default Conference_track
