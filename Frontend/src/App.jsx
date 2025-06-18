e98f16e0fa49355816f785cc0d969bdf5ef96a47
Rup-code <biswarupmaji.23@nshm.edu.in>
2025-06-18 13:20:40 +0530

    create route EditorialBoardPage

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,20 +5,22 @@ import Conference_track from "./Paages/Conference_track";
 import ProgramCommittee from "./Paages/ProgramCommittee";
 import AboutConferencePage from "./Paages/AboutConferencePage";
 import KeynoteSpeakerPage from "./Paages/KeynoteSpeakerPage";
+import EditorialBoardPage from "./Paages/EditorialBoardPage";
 
 function App() {
   return (
     <div className="bg-[var(--cream)] overflow-hidden">
       <Routes>
-      <Route path="/" element={<Home />} />
-      <Route
-        path="/Conference-Tracks-and-Sessions"
-        element={<Conference_track />}
-      />
-      <Route path="/Program-Committee" element={<ProgramCommittee />} />
-      <Route path="/about-conference" element ={<AboutConferencePage/>} />
-      <Route path="/Keynote-Speakers" element ={<KeynoteSpeakerPage/>} />
-    </Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/Conference-Tracks-and-Sessions"
+          element={<Conference_track />}
+        />
+        <Route path="/Program-Committee" element={<ProgramCommittee />} />
+        <Route path="/about-conference" element={<AboutConferencePage />} />
+        <Route path="/Keynote-Speakers" element={<KeynoteSpeakerPage />} />
+        <Route path="/Editorial-Board" element={<EditorialBoardPage />} />
+      </Routes>
     </div>
   );
 }
---
+++ Frontend/src/Paages/EditorialBoardPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function EditorialBoardPage() {
+  return (
+    <div>EditorialBoardPage</div>
+  )
+}
+
+export default EditorialBoardPage
